6b1f26f519730535560bd9959784bbfb939d0285
slolow <laszlostarost@googlemail.com>
2021-01-19 17:49:47 +0100

    This closes #2

--- src/test/java/de/unikassel/soc/platform/services/CustomerServiceImplTest.java
+++ src/test/java/de/unikassel/soc/platform/services/CustomerServiceImplTest.java
@@ -1,17 +1,17 @@
 package de.unikassel.soc.platform.services;
 
 import de.unikassel.soc.platform.domain.Customer;
-import de.unikassel.soc.platform.domain.Product;
 import de.unikassel.soc.platform.repos.CustomerRepo;
 import de.unikassel.soc.platform.web.mappers.CustomerMapper;
 import de.unikassel.soc.platform.web.mappers.CustomerMapperImpl;
-import org.junit.jupiter.api.AfterEach;
+import de.unikassel.soc.platform.web.model.CustomerDto;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.List;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -24,6 +24,7 @@ class CustomerServiceImplTest {
 
     static CustomerMapper mapper;
     CustomerServiceImpl customerService;
+    UUID uuid;
 
     @Mock
     CustomerRepo repo;
@@ -37,20 +38,45 @@ class CustomerServiceImplTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         customerService = new CustomerServiceImpl(repo, mapper);
+        uuid = UUID.randomUUID();
     }
 
     @Test
     void getCustomerByIdError() {
-        UUID uuid = UUID.randomUUID();
+        //UUID uuid = UUID.randomUUID();
         assertThrows(NoSuchElementException.class, () -> customerService.getCustomerById(uuid));
     }
 
     @Test
     void getCustomerById() {
-        UUID uuid = UUID.randomUUID();
-        Customer customer = new Customer(uuid, "Test", new ArrayList<Product>());
+        //UUID uuid = UUID.randomUUID();
+        Customer customer = new Customer(uuid, "Test", new ArrayList<>());
         when(repo.findById(uuid)).thenReturn(Optional.of(customer));
         assertEquals(customer.getName(), customerService.getCustomerById(uuid).getName());
         verify(repo, times(1)).findById(uuid);
+    }
+
+    @Test
+    void getCustomerByName() {
+        List<Customer> customers = new ArrayList<>();
+        List<CustomerDto> customersDto = new ArrayList<>();
+
+        for (int i = 0; i < 2; i++) {
+            UUID uuid = UUID.randomUUID();
+            Customer customer = new Customer(uuid, "Hans", new ArrayList<>());
+            customers.add(customer);
+            customersDto.add(mapper.customerToCustomerDto(customer));
+        }
+        String customerName = customers.get(0).getName();
+        when(repo.findByName(customerName)).thenReturn(customers);
+        assertEquals(customersDto, customerService.getCustomersByName("Hans"));
+        verify(repo, times(1)).findByName(customerName);
+    }
+
+    @Test
+    void updateCustomer() {
+        CustomerDto customerDto = new CustomerDto(uuid, "Hans", new ArrayList<>());
+        customerService.updateCustomer(uuid, customerDto);
+        verify(repo, times(1)).save(mapper.customerDtoToCustomer(customerDto));
     }
 }
